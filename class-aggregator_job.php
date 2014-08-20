d83ba0c211f3f2aeb2b363d473db2e893500b411
Philip John <phil@philipjohn.co.uk>
2014-08-20 20:05:52 +0100

    Additional properties/methods

--- class-aggregator_job.php
+++ class-aggregator_job.php
@@ -9,19 +9,39 @@
 Class Aggregator_Job {
 
 	/**
-	 * @var ID of the portal blog
+	 * @var int ID of the portal blog
 	 */
 	public $portal;
 
 	/**
-	 * @var ID of the source blog
+	 * @var int ID of the source blog
 	 */
 	public $source;
 
 	/**
-	 * @var ID of the blog we're currently on
+	 * @var int ID of the post used to store sync settings
+	 */
+	protected $post_id;
+
+	/**
+	 * @var int ID of the blog we're currently on
 	 */
 	protected $current_blog;
+
+	/**
+	 * @var array Post types to sync
+	 */
+	protected $post_types = array();
+
+	/**
+	 * @var array Taxonomies to sync
+	 */
+	protected $taxonomies = array();
+
+	/**
+	 * @var array Taxonomy terms to sync
+	 */
+	protected $terms = array();
 
 	/**
 	 * Set up all the things
@@ -35,9 +55,13 @@ Class Aggregator_Job {
 
 		// Get the current blog ID ($this->current_blog)
 
-		// Get the details of the portal site for this sync job and store in $this->portal
+		// Get the details of the portal blog for this job and store in $this->portal
 
-		// Get the details of the source of posts for this job and store in $this->source
+		// Get the details of the source blog for this job and store in $this->source
+
+		// Switch to source blog and fetch the settings storing them in properties
+
+		// Store in $this->post_id the ID of the custom post type used to store info
 
 	}
 
@@ -75,6 +99,21 @@ Class Aggregator_Job {
 	}
 
 	/**
+	 * Set the post types that are to be included in this job
+	 *
+	 * @param array $post_types An array of post type names
+	 *
+	 * @return bool Report on success or failure
+	 */
+	public function set_post_types( $post_types ) {
+
+		// Update the post meta of $this->post_id
+
+		// Update $this->post_types
+
+	}
+
+	/**
 	 * Get a list of taxonomies to sync under this job
 	 *
 	 * @return array An array of taxonomy names
@@ -82,6 +121,21 @@ Class Aggregator_Job {
 	public function get_taxonomies() {
 
 		return array();
+
+	}
+
+	/**
+	 * Set the taxonomies that are to be included in this job
+	 *
+	 * @param array $taxonomies Array of taxonomy names
+	 *
+	 * @return bool Report on success or failure
+	 */
+	public function set_taxonomies( $taxonomies ) {
+
+		// Update the taxonomies meta field for $this->post_id
+
+		// Update $this->taxonomies
 
 	}
 
@@ -99,6 +153,21 @@ Class Aggregator_Job {
 	}
 
 	/**
+	 * Set the terms that are to be included in this job
+	 *
+	 * @param array $terms Array of taxonomy => terms
+	 *
+	 * @return bool Success (true) or failure (false)
+	 */
+	public function set_terms( $terms ) {
+
+		// Update the terms for $this->post_id
+
+		// Update $this->terms
+
+	}
+
+	/**
 	 * Get the author to use on the portal site for all pushed posts
 	 *
 	 * @return int ID of the author
@@ -106,6 +175,21 @@ Class Aggregator_Job {
 	public function get_author() {
 
 		return 1;
+
+	}
+
+	/**
+	 * Set the author that is to be assigned to pushed posts on the portal
+	 *
+	 * @param int $author_id ID of an author on the portal site
+	 *
+	 * @return bool Success (true) or failure (false)
+	 */
+	public function set_author( $author_id ) {
+
+		// Update the author post meta for $this->post_id
+
+		// Update $this->author
 
 	}
 

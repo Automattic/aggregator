440a22621ca6f728d6cca849574c9a4f84981a61
Philip John <phil@philipjohn.co.uk>
2014-08-22 17:10:30 +0100

    Job setup

--- class-aggregator_job.php
+++ class-aggregator_job.php
@@ -57,21 +57,51 @@ Class Aggregator_Job {
 	public function __construct( $portal, $source ) {
 
 		// Validate portal and source IDs
+		if ( ! intval( $portal ) || ! intval( $source ) )
+			return;
 
 		// Get the current blog ID ($this->current_blog)
+		$this->current_blog = get_current_blog_id();
 
 		// Get the details of the portal blog for this job and store in $this->portal
+		$this->portal = get_blog_details( $portal );
 
 		// Get the details of the source blog for this job and store in $this->source
+		$this->source = get_blog_details( $source );
 
 		// Switch to source blog if not already on it
+		$this->switch_to_blog( $source );
 
 		// Fetch the post of type 'aggregator_job' where;
 		//  * post meta 'portal' is equal to $portal
+		$jobs_query = new WP_Query( array(
+			'post_type' => 'aggregator_job',
+			'meta_key' => '_aggregator_portal',
+			'meta_value' => $this->get_portal_blog_id(),
+			'posts_per_page' => 1,
+		) );
+		if ( $jobs_query->have_posts() ) {
+			while ( $jobs_query->have_posts() ) {
+				$jobs_query->the_post();
 
-		// Store in $this->post_id the ID of the custom post type used to store info
+				// Store the post ID for later
+				$this->post_id = get_the_ID();
+
+				// Store post types for later
+				$this->post_types = get_post_meta( get_the_ID(), '_aggregator_post_types', true );
+
+				// Store taxonomies for later
+				$this->taxonomies = get_post_meta( get_the_ID(), '_aggregator_taxonomies', true );
+
+				// Store terms for later
+				$this->terms = wp_get_object_terms( get_the_ID(), $this->taxonomies );
+			}
+		}
+
+		$this->restore_current_blog();
 
 		// Create a "Job ID" using source and post IDs
+		$this->job_id = $this->get_source_blog_id() . '_' . $this->post_id;
 
 	}
 
@@ -87,11 +117,33 @@ Class Aggregator_Job {
 	}
 
 	/**
+	 * Get the ID of the portal blog.
+	 *
+	 * @return int ID of the portal blog
+	 */
+	public function get_portal_blog_id() {
+
+		return $this->portal->blog_id;
+
+	}
+
+	/**
 	 * Get the name of the source blog, for use in admin screens
 	 *
 	 * @return string Name of the source blog
 	 */
 	public function get_source_blog_name() {
+
+		return '';
+
+	}
+
+	/**
+	 * Get the ID of the source blog.
+	 *
+	 * @return int ID of the source blog
+	 */
+	public function get_source_blog_id() {
 
 		return '';
 
@@ -215,7 +267,7 @@ Class Aggregator_Job {
 		$url = get_edit_post_link( $this->post_id );
 
 		// Add the portal ID as a parameter to the URL
-		$url = add_query_arg( 'portal', $this->portal, $url );
+		$url = add_query_arg( 'portal', $this->get_portal_blog_id(), $url );
 
 		return esc_url( $url );
 
@@ -233,6 +285,20 @@ Class Aggregator_Job {
 		$url = get_delete_post_link( $this->post_id, '', true );
 
 		return esc_url( $url );
+
+	}
+
+	protected function switch_to_blog( $blog_id ) {
+
+		if ( $this->current_blog != $blog_id )
+			switch_to_blog( $blog_id );
+
+	}
+
+	protected function restore_current_blog() {
+
+		if ( get_current_blog_id() != $this->current_blog )
+			restore_current_blog();
 
 	}
 

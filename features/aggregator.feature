bffde9a70efad7b22b1578334cc57d284a149874
Philip John <phil@philipjohn.co.uk>
2014-12-15 21:17:54 +0000

    Our tests

---
+++ features/aggregator.feature
@@ -0,0 +1,8 @@
+Feature: Aggregator
+  In order to use Aggregator
+  As a super admin
+  I need to be able to manage aggregation jobs
+
+  @javascript
+  Scenario: Access the Aggregator admin screen
+    Given I am logged into WordPress with username "admin" and password "password"
---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,59 @@
+<?php
+
+use Behat\Behat\Context\ClosuredContextInterface,
+	Behat\Behat\Context\TranslatedContextInterface,
+	Behat\Behat\Context\BehatContext,
+	Behat\Behat\Exception\PendingException;
+use Behat\Gherkin\Node\PyStringNode,
+	Behat\Gherkin\Node\TableNode;
+
+use Behat\MinkExtension\Context\MinkContext;
+use WebDriver\Exception\NoAlertOpenError;
+
+/**
+ * Features context.
+ */
+class FeatureContext extends MinkContext {
+    /**
+     * Initializes context.
+     * Every scenario gets it's own context object.
+     *
+     * @param array $parameters context parameters (set them up through behat.yml)
+     */
+    public function __construct( array $parameters ) {
+        // Initialize your context here
+    }
+
+	/**
+	 * @Given /^I am logged into WordPress with username "([^"]*)" and password "([^"]*)"$/
+	 */
+	public function iAmLoggedIntoWordpressWithUsernameAndPassword( $username, $password ) {
+
+		$session = $this->getSession();
+		if ( ! $session->isStarted() ) {
+			$session->start();
+		}
+
+		// Check that the driver is Selenium, not Goutte
+		if ( ! is_a( $session->getDriver(), 'Behat\Mink\Driver\Selenium2Driver' ) ) {
+			throw new \Exception( "Can't log in unless you're using Selenium driver" );
+		}
+
+		// Fill in the WordPress login form and submit
+		$session->visit( $session->getCurrentUrl().'/wp-admin' );
+		$this->fillField( 'user_login', $username );
+		$this->fillField( 'user_pass', $password );
+		$this->pressButton( 'Log In' );
+
+		// Check login didn't fail
+		$assert = $this->assertSession();
+		$assert->pageTextNotContains( 'Invalid username.' );
+		$assert->pageTextNotContains( 'ERROR: The password you entered for the' );
+
+		// Check we're on the dashboard
+		$assert->pageTextContains( 'Dashboard' );
+
+	}
+
+
+}

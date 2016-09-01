f13991c787e9f85712db21864a6575e010a98b2f
Philip John <phil@philipjohn.co.uk>
2016-09-01 20:10:52 +0100

    PHPCS fixes for class-aggregator-job.php including file renaming

--- class-aggregator-job.php
+++ class-aggregator-job.php
@@ -1,120 +1,107 @@
 <?php
-
-/*
-  Copyright 2014 Code for the People Ltd
-
-                _____________
-               /      ____   \
-         _____/       \   \   \
-        /\    \        \___\   \
-       /  \    \                \
-      /   /    /          _______\
-     /   /    /          \       /
-    /   /    /            \     /
-    \   \    \ _____    ___\   /
-     \   \    /\    \  /       \
-      \   \  /  \____\/    _____\
-       \   \/        /    /    / \
-        \           /____/    /___\
-         \                        /
-          \______________________/
-
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
-*/
+/**
+ * File contains the Aggregator Job class
+ *
+ * @package Aggregator
+ */
 
 /**
  * Defines an Aggregator job.
  *
  * Serves details of the post types, taxonomies and terms to be synced between
- * two specified blogs in a multisite network
+ * two specified blogs in a multisite network.
  */
 class Aggregator_Job {
 
 	/**
-	 * @var int ID of the portal blog
+	 * ID of the portal blog
+	 *
+	 * @var int
 	 */
 	public $portal;
 
 	/**
-	 * @var int ID of the source blog
+	 * ID of the source blog
+	 *
+	 * @var int
 	 */
 	public $source;
 
 	/**
-	 * @var int ID of the post used to store sync settings
+	 * ID of the post used to store sync settings
+	 *
+	 * @var int
 	 */
 	public $post_id;
 
 	/**
-	 * @var int ID of the blog we're currently on
+	 * ID of the blog we're currently on
+	 *
+	 * @var int
 	 */
 	protected $current_blog;
 
 	/**
-	 * @var array Post types to sync
+	 * Post types to sync
+	 *
+	 * @var array
 	 */
 	protected $post_types = array();
 
 	/**
-	 * @var array Taxonomies to sync
+	 * Taxonomies to sync
+	 *
+	 * @var array
 	 */
 	protected $taxonomies = array();
 
 	/**
-	 * @var array Taxonomy terms to sync
+	 * Taxonomy terms to sync
+	 *
+	 * @var array
 	 */
 	protected $terms = array();
 
 	/**
-	 * @var int Author ID for the user to whom pushed posts should be assigned
+	 * Author ID for the user to whom pushed posts should be assigned
+	 *
+	 * @var int
 	 */
 	protected $author = 1;
 
 	/**
-	 * @var string An ID constructed as {$source_id}_{$post_id}
+	 * An ID constructed as {$source_id}_{$post_id}
+	 *
+	 * @var string
 	 */
 	public $job_id;
 
 	/**
 	 * Set up all the things
 	 *
-	 * @param int $portal ID of the portal blog
-	 * @param int $source ID of the source blog
+	 * @param int $portal ID of the portal blog.
+	 * @param int $source ID of the source blog.
 	 */
 	public function __construct( $portal, $source ) {
 
-		// Validate portal and source IDs
+		// Validate portal and source IDs.
 		if ( ! intval( $portal ) || ! intval( $source ) ) {
 			return; }
 
-		// Get the current blog ID ($this->current_blog)
+		// Get the current blog ID ($this->current_blog).
 		$this->current_blog = get_current_blog_id();
 
-		// Get the details of the portal blog for this job and store in $this->portal
+		// Get the details of the portal blog for this job and store in $this->portal.
 		$this->portal = get_blog_details( $portal );
 
-		// Get the details of the source blog for this job and store in $this->source
+		// Get the details of the source blog for this job and store in $this->source.
 		$this->source = get_blog_details( $source );
 
-		// Switch to source blog if not already on it
+		// Switch to source blog if not already on it.
 		$this->switch_to_blog( $source );
 
 		// Fetch the post of type 'aggregator_job' where;
-		// * post meta 'portal' is equal to $portal
+		// * post meta 'portal' is equal to $portal.
 		$jobs_query = new WP_Query( array(
 			'post_type' => 'aggregator_job',
 			'meta_key' => '_aggregator_portal',
@@ -126,26 +113,26 @@ class Aggregator_Job {
 			while ( $jobs_query->have_posts() ) {
 				$jobs_query->the_post();
 
-				// Store the post ID for later
+				// Store the post ID for later.
 				$this->post_id = get_the_ID();
 
-				// Store post types for later
+				// Store post types for later.
 				$this->post_types = get_post_meta( get_the_ID(), '_aggregator_post_types', true );
 
-				// Store taxonomies for later
+				// Store taxonomies for later.
 				$this->taxonomies = get_post_meta( get_the_ID(), '_aggregator_taxonomies', true );
 
-				// Store terms for later
+				// Store terms for later.
 				$this->terms = $this->set_terms( wp_get_object_terms( get_the_ID(), $this->taxonomies ) );
 
-				// Retrieve the author ID for the portal
+				// Retrieve the author ID for the portal.
 				$this->author = get_post_meta( get_the_ID(), '_aggregator_author', true );
 			}
 		}
 
 		$this->restore_current_blog();
 
-		// Create a "Job ID" using source and post IDs
+		// Create a "Job ID" using source and post IDs.
 		$this->job_id = $this->get_source_blog_id() . '_' . $this->post_id;
 
 	}
@@ -179,13 +166,13 @@ class Aggregator_Job {
 	 */
 	public function set_portal_blog_id_meta() {
 
-		// Maybe switch to blog
+		// Maybe switch to blog.
 		$this->switch_to_blog( $this->get_source_blog_id() );
 
-		// Update the post meta
+		// Update the post meta.
 		update_post_meta( $this->post_id, '_aggregator_portal', $this->get_portal_blog_id() );
 
-		// Maybe restore current blog
+		// Maybe restore current blog.
 		$this->restore_current_blog();
 
 	}
@@ -226,22 +213,20 @@ class Aggregator_Job {
 	/**
 	 * Set the post types that are to be included in this job
 	 *
-	 * @param array $post_types An array of post type names
-	 *
-	 * @return bool Report on success or failure
+	 * @param array $post_types An array of post type names.
 	 */
 	public function set_post_types( $post_types ) {
 
-		// Maybe switch to blog
+		// Maybe switch to blog.
 		$this->switch_to_blog( $this->get_source_blog_id() );
 
-		// Update the post meta of $this->post_id
+		// Update the post meta of $this->post_id.
 		update_post_meta( $this->post_id, '_aggregator_post_types', $post_types );
 
-		// Update $this->post_types
+		// Update $this->post_types.
 		$this->post_types = $post_types;
 
-		// Maybe restore current blog
+		// Maybe restore current blog.
 		$this->restore_current_blog();
 
 	}
@@ -260,22 +245,20 @@ class Aggregator_Job {
 	/**
 	 * Set the taxonomies that are to be included in this job
 	 *
-	 * @param array $taxonomies Array of taxonomy names
-	 *
-	 * @return bool Report on success or failure
+	 * @param array $taxonomies Array of taxonomy names.
 	 */
 	public function set_taxonomies( $taxonomies ) {
 
-		// Maybe switch to blog
+		// Maybe switch to blog.
 		$this->switch_to_blog( $this->get_source_blog_id() );
 
-		// Update the taxonomies meta field for $this->post_id
+		// Update the taxonomies meta field for $this->post_id.
 		update_post_meta( $this->post_id, '_aggregator_taxonomies', $taxonomies );
 
-		// Update $this->taxonomies
+		// Update $this->taxonomies.
 		$this->taxonomies = $taxonomies;
 
-		// Maybe restore current blog
+		// Maybe restore current blog.
 		$this->restore_current_blog();
 
 	}
@@ -283,13 +266,13 @@ class Aggregator_Job {
 	/**
 	 * Get a list of terms to sync under this job
 	 *
-	 * @param string $taxonomy The taxonomy whose terms to return
+	 * @param string $taxonomy The taxonomy whose terms to return.
 	 *
 	 * @return array An array of term names
 	 */
 	public function get_terms( $taxonomy = 'aggregator_term_count' ) {
 
-		// $this-terms is an array of Term objects
+		// $this-terms is an array of Term objects.
 		if ( array_key_exists( $taxonomy, $this->terms ) ) {
 			return $this->terms[ $taxonomy ]; }
 
@@ -322,11 +305,13 @@ class Aggregator_Job {
 	 * 		)
 	 * 	)
 	 *
-	 * @param array $terms Array of WP_Term objects
+	 * @param array $terms Array of WP_Term objects.
+	 *
+	 * @return array $store_terms Array of terms, grouped by taxonomy
 	 */
 	protected function set_terms( $terms ) {
 
-		// Store terms
+		// Store terms.
 		$store_terms = array();
 
 		foreach ( $terms as $term ) {
@@ -351,26 +336,26 @@ class Aggregator_Job {
 	/**
 	 * Set the author that is to be assigned to pushed posts on the portal
 	 *
-	 * @param int $author_id ID of an author on the portal site
+	 * @param int $author_id ID of an author on the portal site.
 	 *
 	 * @return bool Success (true) or failure (false)
 	 */
 	public function set_author( $author_id ) {
 
-		// Check it's an integer
+		// Check it's an integer.
 		if ( ! intval( $author_id ) ) {
 			return; }
 
-		// Maybe switch to blog
+		// Maybe switch to blog.
 		$this->switch_to_blog( $this->get_source_blog_id() );
 
-		// Update the taxonomies meta field for $this->post_id
+		// Update the taxonomies meta field for $this->post_id.
 		update_post_meta( $this->post_id, '_aggregator_author', $author_id );
 
-		// Update $this->author
+		// Update $this->author.
 		$this->author = $author_id;
 
-		// Maybe restore current blog
+		// Maybe restore current blog.
 		$this->restore_current_blog();
 
 	}
@@ -384,16 +369,16 @@ class Aggregator_Job {
 	 */
 	public function get_edit_post_link() {
 
-		// Maybe switch to blog
+		// Maybe switch to blog.
 		$this->switch_to_blog( $this->source->blog_id );
 
-		// Get the edit link
+		// Get the edit link.
 		$url = get_edit_post_link( $this->post_id );
 
-		// Add the portal ID as a parameter to the URL
+		// Add the portal ID as a parameter to the URL.
 		$url = add_query_arg( 'portal', $this->get_portal_blog_id(), $url );
 
-		// Switch back
+		// Switch back.
 		$this->restore_current_blog();
 
 		return esc_url( $url );
@@ -419,23 +404,38 @@ class Aggregator_Job {
 
 	}
 
+	/**
+	 * Helper for switch_to_blog().
+	 *
+	 * @param int $blog_id ID of the blog to switch to.
+	 */
 	protected function switch_to_blog( $blog_id ) {
 
-		if ( $this->current_blog != $blog_id ) {
-			switch_to_blog( $blog_id ); }
+		if ( $this->current_blog !== $blog_id ) {
+			switch_to_blog( $blog_id );
+		}
 
 	}
 
+	/**
+	 * Helper for restore_current_blog().
+	 */
 	protected function restore_current_blog() {
 
-		if ( get_current_blog_id() != $this->current_blog ) {
-			restore_current_blog(); }
+		if ( get_current_blog_id() !== $this->current_blog ) {
+			restore_current_blog();
+		}
 
 	}
 
+	/**
+	 * Set the ID of the post for later use.
+	 *
+	 * @param int $post_id ID of the post.
+	 */
 	public function set_post_id( $post_id ) {
 
-		// Set the post ID
+		// Set the post ID.
 		$this->post_id = intval( $post_id );
 
 	}
@@ -446,55 +446,54 @@ class Aggregator_Job {
 	 * Sets/update the network option telling us which sources a portal will receive posts from, and
 	 * our network option denoting which portals a source will push to.
 	 *
-	 * @param int $portal ID of the portal blog
-	 * @param int $source ID of the source blog
+	 * @param string $action The action to perform - add|delete.
 	 */
 	public function update_network_options( $action = 'add' ) {
 
-		// Validate
+		// Validate.
 		$portal = intval( $this->get_portal_blog_id() );
 		$source = intval( $this->get_source_blog_id() );
 
-		// Get list of existing sources for this portal
+		// Get list of existing sources for this portal.
 		$sources = get_site_option( "aggregator_{$portal}_source_blogs", array() );
 
-		// Get list of existing portals for this source
+		// Get list of existing portals for this source.
 		$portals = get_site_option( "aggregator_{$source}_portal_blogs", array() );
 
-		// Decide what to do
+		// Decide what to do.
 		switch ( $action ) {
 
-			// Remove sites
+			// Remove sites.
 			case 'delete':
 
-				// Delete this source, if present
+				// Delete this source, if present.
 				$source_found = array_search( $source, $sources );
-				if ( $source_found !== false ) {
+				if ( false !== $source_found ) {
 					unset( $sources[ $source_found ] ); }
 
-				// Delete this portal if present
+				// Delete this portal if present.
 				$portal_found = array_search( $portal, $portals );
-				if ( $portal_found !== false ) {
+				if ( false !== $portal_found ) {
 					unset( $portals[ $portal_found ] ); }
 
 				break;
 
-			// Add sites
+			// Add sites.
 			case 'add':
 
-				// Add this source, if not already added
-				if ( ! in_array( $source, $sources ) ) {
+				// Add this source, if not already added.
+				if ( ! in_array( $source, $sources, true ) ) {
 					$sources[] = $source; }
 
-				// Add this portal if not already added
-				if ( ! in_array( $portal, $portals ) ) {
+				// Add this portal if not already added.
+				if ( ! in_array( $portal, $portals, true ) ) {
 					$portals[] = $portal; }
 
 				break;
 
 		}
 
-		// Update the options with our new values
+		// Update the options with our new values.
 		$sources = update_site_option( "aggregator_{$portal}_source_blogs", $sources );
 		$portals = update_site_option( "aggregator_{$source}_portal_blogs", $portals );
 
@@ -505,16 +504,16 @@ class Aggregator_Job {
 	 */
 	public function delete_job() {
 
-		// Switch to the source blog
+		// Switch to the source blog.
 		$this->switch_to_blog( $this->get_source_blog_id() );
 
-		// Delete the job post
+		// Delete the job post.
 		wp_delete_post( $this->post_id );
 
-		// Switch back
+		// Switch back.
 		$this->restore_current_blog();
 
-		// Update the network options
+		// Update the network options.
 		$this->update_network_options( 'delete' );
 
 	}

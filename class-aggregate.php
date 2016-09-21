780a4d2d8fa0ade24312cdcf274d24a66271b499
Philip John <phil@philipjohn.co.uk>
2016-09-21 21:03:23 +0100

    Sync the original post status, with filter.
    Ensures the original status of the post is also synced to the child site. A filter (`aggregator_post_status`) is added so that the status can be changed by any plugin or theme, as desired.

--- class-aggregate.php
+++ class-aggregate.php
@@ -797,7 +797,6 @@ class Aggregate extends Aggregator_Plugin {
 				wp_update_post( $orig_post_data );
 			} else {
 				$target_post_id = wp_insert_post( $orig_post_data );
-				$orig_post_data['ID'] = $target_post_id;
 			}
 
 			// Push the meta data.
@@ -844,6 +843,22 @@ class Aggregate extends Aggregator_Plugin {
 				);
 				// @codingStandardsIgnoreEnd
 			}
+
+			// Finally, set the post status.
+			$new_post_data = get_post( $target_post_id );
+
+			/**
+			 * Filter the status of a syncing post.
+			 *
+			 * Allows for the status of a synced post to be overridden. Before
+			 * this filter runs, the status will be 'pending'. The value of
+			 * `$status` is the status of the original post being synced, which
+			 * will be passed to `wp_update_post()`.
+			 *
+			 * @var string $status The final status of the post.
+			 */
+			$new_post_data->post_status = apply_filters( 'aggregator_post_status', $orig_post_data['post_status'] );
+			wp_update_post( $new_post_data );
 
 			// Switch back to source blog.
 			restore_current_blog();
